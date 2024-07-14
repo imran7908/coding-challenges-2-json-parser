beef11b64f5ace471606d1a54e6e5f4dbbaafaa0
imran7908 <imran1510043@gmail.com>
2024-07-14 16:10:35 +0600

    Validate test file extension

--- index.js
+++ index.js
@@ -1,1 +1,13 @@
-console.log("Hello World");
+import * as path from "node:path";
+import { exit } from "node:process";
+
+export const jsonParser = (file) => {
+  const extension = path.extname(file);
+  if (extension !== ".json") {
+    console.log("This file is not a json file");
+    exit(1);
+  }
+  exit(0);
+};
+
+jsonParser("testfile");
