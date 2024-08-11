d3f4984288edb124cff7ba092ad45e7fbc307299
imran7908 <imran1510043@gmail.com>
2024-08-11 17:59:31 +0600

    Check json is array or object

--- index.js
+++ index.js
@@ -2,14 +2,29 @@ import * as path from "node:path";
 import * as fs from "node:fs/promises";
 import { exit } from "node:process";
 
-export const jsonParser = async (file) => {
-  const extension = path.extname(file);
-  if (extension !== ".json") {
+const jsonParser = async (file) => {
+  const fileExtension = path.extname(file);
+  if (fileExtension !== ".json" && fileExtension !== ".JSON") {
     console.log("This file is not a json file");
     exit(1);
   }
 
   const contents = await fs.readFile(file, { encoding: "utf-8" });
+  const trimmedContents = contents.trim();
+
+  if (
+    trimmedContents[0] === "{" &&
+    trimmedContents[trimmedContents.length - 1] === "}"
+  ) {
+    console.log("It is a object");
+  }
+
+  if (
+    trimmedContents[0] === "[" &&
+    trimmedContents[trimmedContents.length - 1] === "]"
+  ) {
+    console.log("It is a array");
+  }
 };
 
 jsonParser("testfile.json");
